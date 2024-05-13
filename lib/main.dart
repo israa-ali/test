d3d73d89cde2ce53f75174cc13e648d46ca1306b
israa-ali <israa18ali99@gmail.com>
2024-05-13 09:44:15 +0200

    test

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tawk/flutter_tawk.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+     
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+ 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("chat"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+      ),
+      body:Center(child: Tawk(
+          directChatLink: 'https://libyapost.ly',
+          visitor: TawkVisitor(
+            name: 'Ayoub AMINE',
+            email: 'ayoubamine2a@gmail.com',
+          ),
+          onLoad: () {
+            print('Hello Tawk!');
+          },
+          onLinkTap: (String url) {
+            print(url);
+          },
+          placeholder: const Center(
+            child: Text('Loading...'),
+          ),
+        ),)
+      /*Tawk(
+          directChatLink: 'https://libyapost.ly',
+          visitor: TawkVisitor(
+            name: 'Ayoub AMINE',
+            email: 'ayoubamine2a@gmail.com',
+          ),
+          onLoad: () {
+            print('Hello Tawk!');
+          },
+          onLinkTap: (String url) {
+            print(url);
+          },
+          placeholder: const Center(
+            child: Text('Loading...'),
+          ),
+        ),*/
+    );
+  }
+}
